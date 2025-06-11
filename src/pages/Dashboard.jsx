0bb416bb2649b44b25ae06b7b49875eb282baafc
Mohammed Atef <mohammed.atef.4200@gmail.com>
2025-06-12 01:30:32 +0300

    update: header

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -46,13 +46,12 @@ const Dashboard = () => {
         <div>
           <MindExecLogo />
         </div>
-        <div className="flex justify-center items-center me-2 text-main ">
+        <div className="flex justify-center items-center gap-x-4 me-2 text-main ">
           <svg
             xmlns="http://www.w3.org/2000/svg"
             width="24"
             height="24"
             viewBox="0 0 24 24"
-            className="me-6"
             fill="none">
             <path
               d="M8.645 20.5C8.86103 21.2219 9.30417 21.8549 9.90858 22.3049C10.513 22.755 11.2464 22.998 12 22.998C12.7536 22.998 13.487 22.755 14.0914 22.3049C14.6958 21.8549 15.139 21.2219 15.355 20.5H8.645ZM3 19.5H21V16.5L19 13.5V8.5C19 7.58075 18.8189 6.6705 18.4672 5.82122C18.1154 4.97194 17.5998 4.20026 16.9497 3.55025C16.2997 2.90024 15.5281 2.38463 14.6788 2.03284C13.8295 1.68106 12.9193 1.5 12 1.5C11.0807 1.5 10.1705 1.68106 9.32122 2.03284C8.47194 2.38463 7.70026 2.90024 7.05025 3.55025C6.40024 4.20026 5.88463 4.97194 5.53284 5.82122C5.18106 6.6705 5 7.58075 5 8.5V13.5L3 16.5V19.5Z"
@@ -65,9 +64,9 @@ const Dashboard = () => {
               fill="#7246A7"
             />
           </svg>
-          <button onClick={signOut}>Log out</button>
-          <p className="p-[6px] me-2 leading-[110%] bg-[#060606] rounded-sm text-[20px]">{getInitials(user?.user.user_metadata.full_name)}</p>
           <p className="text-[18px]">{capitalize(user?.user.user_metadata.full_name)}</p>
+          <p className="p-[6px] me-2 leading-[110%] bg-[#060606] rounded-sm text-[20px]">{getInitials(user?.user.user_metadata.full_name)}</p>
+          <button onClick={signOut} className="text-red-500">Log out</button>
         </div>
       </nav>
       <div className="flex">
@@ -303,7 +302,7 @@ const Dashboard = () => {
                 </defs>
               </svg>
               <h2 className="text-white text-xl mb-4">Start your Automation Journey </h2>
-              <p className="text-[#A0A0A0] mb-8 text-center w-4/5 text-sm leading-[17px]">Start your journey by executing your first run. Our Workflow Library offers a variety of ready-to-launch workflows. It&apos;s designed to help you start quickly without the need to build from scratch. Need help? Check out our guides on executing a workflow and using workflows from the library</p>
+              <p className="text-muted mb-8 text-center w-4/5 text-sm ">Start your journey by executing your first run. Our Workflow Library offers a variety of ready-to-launch workflows. It&apos;s designed to help you start quickly without the need to build from scratch. Need help? Check out our guides on executing a workflow and using workflows from the library</p>
               <button className="px-6 py-3  rounded-lg text-xl text-main bg-[#360077]">Explore Workflows</button>
             </div>
             <div className="w-2/5 border-2 rounded-lg border-red-primary shadow-red-primary flex flex-col  items-center">
