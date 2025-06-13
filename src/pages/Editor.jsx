42ff9fc625923b332c5e94269a2dfbab2e7db73c
Mohammed Atef <mohammed.atef.4200@gmail.com>
2025-06-13 04:58:23 +0300

    refactor: editor code

--- src/pages/Editor.jsx
+++ src/pages/Editor.jsx
@@ -1,6 +1,3 @@
-import { useContext } from "react";
-import AppContext from "../AppContext";
-import ArrowIcon from "../components/icons/ArrowIcon";
 import LeftFrame from "../components/layout/LeftFrame";
 import MindMap from "../components/layout/MindMap";
 import Nav from "../components/layout/Nav";
@@ -8,69 +5,33 @@ import RightFrame from "../components/layout/RightFrame";
 import { Panel, PanelGroup, PanelResizeHandle } from "react-resizable-panels";
 
 const Editor = () => {
-  const ctx = useContext(AppContext);
-
-  const toggleLeft = () => ctx.setShowLeft(!ctx.showLeft);
-  const toggleRight = () => ctx.setShowRight(!ctx.showRight);
-
-  // const getGridTemplateColumns = () => {
-  //   if (ctx.showLeft && ctx.showRight) return "1fr 3fr 1fr";
-  //   if (ctx.showLeft && !ctx.showRight) return "1fr 4fr";
-  //   if (!ctx.showLeft && ctx.showRight) return "4fr 1fr";
-  //   return "1fr"; // Only the center section is visible
-  // };
 
   return (
     <main className="max-h-screen overflow-hidden">
       <Nav />
       <section className="w-full h-[calc(100vh-81px)]">
         <PanelGroup direction="horizontal">
-          {ctx.showLeft && (
-            <Panel>
-              <div className="relative h-full">
-                <LeftFrame />
-                <button
-                  className="absolute top-1/2 z-20 p-2 rounded-lg text-white -right-5 transform scale-150 -translate-y-1/2 opacity-60 hover:opacity-100 transition-opacity"
-                  onClick={toggleLeft}>
-                  {ctx.showLeft ? <ArrowIcon className=" transform rotate-90" /> : <ArrowIcon className="transform -rotate-90" />}
-                </button>
-              </div>
-            </Panel>
-          )}
+          <Panel>
+            <div className="relative h-full">
+              <LeftFrame />
+            </div>
+          </Panel>
 
-          <PanelResizeHandle />
+          <PanelResizeHandle className="w-px bg-primary-light" />
+
           <Panel>
             <div className="relative h-full">
               <MindMap />
-              {!ctx.showLeft && (
-                <button
-                  className="absolute top-1/2 z-20 p-2 rounded-lg text-white left-0 transform scale-150 -translate-y-1/2 opacity-60 hover:opacity-100 transition-opacity"
-                  onClick={toggleLeft}>
-                  {ctx.showLeft ? <ArrowIcon className=" transform rotate-90" /> : <ArrowIcon className="transform -rotate-90" />}
-                </button>
-              )}
-              {!ctx.showRight && (
-                <button
-                  className="absolute top-1/2 z-20 p-2 rounded-lg text-white right-0 transform scale-150 -translate-y-1/2 opacity-60 hover:opacity-100 transition-opacity"
-                  onClick={toggleRight}>
-                  {ctx.showRight ? <ArrowIcon className=" transform -rotate-90" /> : <ArrowIcon className="transform rotate-90" />}
-                </button>
-              )}
             </div>
           </Panel>
-          <PanelResizeHandle />
-          {ctx.showRight && (
-            <Panel className="bg-gray-100">
-              <div className="relative h-full">
-                <RightFrame />
-                <button
-                  className="absolute top-1/2 z-20 p-2 rounded-lg text-white -left-5 transform scale-150 -translate-y-1/2 opacity-60 hover:opacity-100 transition-opacity"
-                  onClick={toggleRight}>
-                  {ctx.showRight ? <ArrowIcon className=" transform -rotate-90" /> : <ArrowIcon className="transform rotate-90" />}
-                </button>
-              </div>
-            </Panel>
-          )}
+
+          <PanelResizeHandle className="w-px bg-primary-light" />
+
+          <Panel className="bg-gray-100">
+            <div className="relative h-full">
+              <RightFrame />
+            </div>
+          </Panel>
         </PanelGroup>
       </section>
     </main>
