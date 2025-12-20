8b265d9ecde48e60b9946325f1d073533c268d67
Mohammed Atef <mohammed.atef.4200@gmail.com>
2025-12-20 21:48:27 +0300

    feat: protect editor route

--- src/App.jsx
+++ src/App.jsx
@@ -43,7 +43,11 @@ const router = createBrowserRouter(
     },
     {
       path: "/editor",
-      element: <Editor />,
+      element: (
+        <ProtectedRoute>
+          <Editor />
+        </ProtectedRoute>
+      ),
     },
   ]
 );
